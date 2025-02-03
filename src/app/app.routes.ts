f19af5251ce41d499acf081bd9100ff5174832d9
Daniela <danielaandrade66@gmail.com>
2025-02-02 22:32:53 -0600

    Create and activate routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AirportDetailsComponent } from './pages/airport-details/airport-details.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'search-airport', component: HomeComponent },
+    { path: 'airport/:iataCode', component: AirportDetailsComponent }
+];
