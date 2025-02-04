2944ad42780f0df941c6fbb12a409f95ae7e6065
Daniela <danielaandrade66@gmail.com>
2025-02-04 10:24:28 -0600

    Create airpot-details.

--- src/app/pages/airport-details/airport-details.component.ts
+++ src/app/pages/airport-details/airport-details.component.ts
@@ -2,17 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AmadeusService } from '../../amadeus.service';
-
-
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-airport-details',
-  imports: [ CommonModule ],
+  imports: [ CommonModule, HeaderComponent],
   templateUrl: './airport-details.component.html',
   styleUrl: './airport-details.component.css'
 })
 export class AirportDetailsComponent implements OnInit {
-  airport: any = null ;
+  airport: any ;
 
   constructor (private route: ActivatedRoute, private amadeusService: AmadeusService ) {}
 
@@ -20,8 +19,11 @@ export class AirportDetailsComponent implements OnInit {
     const iataCode = this.route.snapshot.paramMap.get('iataCode');
     if ( iataCode ) {
       this.amadeusService.getAirportByIata(iataCode).subscribe( data => {
-        this.airport = data;
+        this.airport = data.data[0]; // Extraer el primer elemento del array
+        console.log(this.airport);
       })
     }
   }
 }
+
+
