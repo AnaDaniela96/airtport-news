bbc659a88341874893f18bb9e898a6dfe41580b6
Daniela <danielaandrade66@gmail.com>
2025-02-04 12:23:23 -0600

    Create components for history and q&a, add this pages on routes and update header.

---
+++ src/app/pages/history/history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+
+@Component({
+  selector: 'app-history',
+  imports: [HeaderComponent],
+  templateUrl: './history.component.html',
+  styleUrl: './history.component.css'
+})
+export class HistoryComponent {
+
+}
---
+++ src/app/pages/questions/questions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+
+@Component({
+  selector: 'app-questions',
+  imports: [HeaderComponent],
+  templateUrl: './questions.component.html',
+  styleUrl: './questions.component.css'
+})
+export class QuestionsComponent {
+
+}
